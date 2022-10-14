cc4aece604e539b3deb7ab59f36239e763ea68db
chandler-geminisports <chandler@geminisports.ai>
2022-10-13 23:38:23 -0400

    add functions to calculate remaining time, compare time to current time for expire logic

--- function/lambda_func.py
+++ function/lambda_func.py
@@ -8,13 +8,16 @@ import datetime
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+class Expired(Exception):
+    pass
+
 def lambda_handler(event, context):
     host = event["host"]
     days = event["days"]
     
     logger.info('SSL Check host: %s with %s days cushion',  host, days)
-    exp_time = get_ssl_expire_time(host)
-    return { 'status': 200, 'expires': json.dumps(exp_time) }
+    exp_bool = is_ssl_cert_expiring(host, days)
+    return { 'status': 200, 'expireWarning': exp_bool }
 
 def get_ssl_expire_time(host):
     context = ssl.create_default_context()
@@ -22,6 +25,22 @@ def get_ssl_expire_time(host):
         with context.wrap_socket(sock, server_hostname = host) as ssock:
             ssl_info = ssock.getpeercert()
     
-    expireTime = ssl_info['notAfter']
+    expire_time = ssl_info['notAfter']
     ssl_date_format = r'%b %d %H:%M:%S %Y %Z'
-    return datetime.datetime.strptime(expireTime, ssl_date_format)
+    return datetime.datetime.strptime(expire_time, ssl_date_format)
+
+def get_ssl_remaining_time(host):
+    expires = get_ssl_expire_time(host)
+    logger.debug('SSL cert for %s expires at %s', host, expires.isoformat())
+    return expires - datetime.datetime.utcnow()
+    
+def is_ssl_cert_expiring(host, days):
+    remaining = get_ssl_remaining_time(host)
+
+    if remaining < datetime.timedelta(days=0):
+        raise Expired("SSL Cert for host %s expired %s days ago" % (host, remaining.days))
+    elif remaining < datetime.timedelta(days=days):
+        logger.warning('Expires in %s days', remaining.days)
+        return True
+    else:
+        return False
