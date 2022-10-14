6761aa5cae93096430a2b5b58739b5dc2d2e456a
chandler-geminisports <chandler@geminisports.ai>
2022-10-13 23:30:43 -0400

    add ssl cert exp function, convert return to http stuff

--- function/lambda_func.py
+++ function/lambda_func.py
@@ -1,6 +1,9 @@
 import os
 import json
 import logging
+import socket
+import ssl
+import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -10,4 +13,15 @@ def lambda_handler(event, context):
     days = event["days"]
     
     logger.info('SSL Check host: %s with %s days cushion',  host, days)
-    return 'Ok'
+    exp_time = get_ssl_expire_time(host)
+    return { 'status': 200, 'expires': json.dumps(exp_time) }
+
+def get_ssl_expire_time(host):
+    context = ssl.create_default_context()
+    with socket.create_connection((host, 443)) as sock:
+        with context.wrap_socket(sock, server_hostname = host) as ssock:
+            ssl_info = ssock.getpeercert()
+    
+    expireTime = ssl_info['notAfter']
+    ssl_date_format = r'%b %d %H:%M:%S %Y %Z'
+    return datetime.datetime.strptime(expireTime, ssl_date_format)
